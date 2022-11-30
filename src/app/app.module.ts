f5edb6abcfaf5dfce7b34f1fea57e9119063584c
fgiardino <fgiardino@aurigaspa>
2022-11-30 17:25:18 +0100

    primo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { it_IT } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import it from '@angular/common/locales/it';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemsComponent } from './items/items.component';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+
+registerLocaleData(it);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzTabsModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: it_IT }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
